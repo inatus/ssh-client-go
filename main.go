c48d73e4ad897bd090116fb9861c2dc12ae06760
Hiroki Inagaki <hiroki@inagaki.in>
2013-08-31 21:41:17 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"code.google.com/p/go.crypto/ssh"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type password string
+
+func (p password) Password(user string) (password string, err error) {
+	return string(p), nil
+}
+
+func main() {
+	fmt.Print("Server? (Default=localhost): ")
+	server := scanConfig()
+	if server == "" {
+		server = "localhost"
+	}
+	fmt.Print("Port? (Default=22): ")
+	port := scanConfig()
+	if port == "" {
+		port = "22"
+	}
+	server = server + ":" + port
+	fmt.Print("UserName?: ")
+	user := scanConfig()
+	fmt.Print("Password?: ")
+	p := scanConfig()
+
+	var pass = password(p)
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.ClientAuth{
+			// ClientAuthPassword wraps a ClientPassword implementation
+			// in a type that implements ClientAuth.
+			ssh.ClientAuthPassword(pass),
+		},
+	}
+	conn, err := ssh.Dial("tcp", server, config)
+	if err != nil {
+		panic("Failed to dial: " + err.Error())
+	}
+	defer conn.Close()
+
+	// Each ClientConn can support multiple interactive sessions,
+	// represented by a Session.
+	session, err := conn.NewSession()
+	if err != nil {
+		panic("Failed to create session: " + err.Error())
+	}
+	defer session.Close()
+
+	// Set IO
+	session.Stdout = os.Stdout
+	in, _ := session.StdinPipe()
+
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		log.Fatalf("request for pseudo terminal failed: %s", err)
+	}
+
+	// Start remote shell
+	if err := session.Shell(); err != nil {
+		log.Fatalf("failed to start shell: %s", err)
+	}
+
+	// Accepting commands
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		str, _ := reader.ReadString('\n')
+		fmt.Fprint(in, str+"\n\x00")
+	}
+
+}
+
+func scanConfig() string {
+	config, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	config = strings.Trim(config, "\n")
+	return config
+}
